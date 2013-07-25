e0a04fc26af9218bf685a19407c18cfdd3c43565
Tyler Garlick <tjgarlick@gmail.com>
2013-07-25 00:22:15 -0400

    remove referece to FlitBit.Ioc, using RedRocket.FlitBit.IoC

--- RedRocket.Persistence.EF/RedRocket.Persistence.EF/IEntityFrameworkRepository.cs
+++ RedRocket.Persistence.EF/RedRocket.Persistence.EF/IEntityFrameworkRepository.cs
@@ -4,7 +4,6 @@ using System.Data;
 using System.Data.Entity;
 using System.Linq;
 using System.Linq.Expressions;
-using RedRocket.Persistence.Common;
 using RedRocket.Utilities.Core.Validation;
 
 namespace RedRocket.Persistence.EF
